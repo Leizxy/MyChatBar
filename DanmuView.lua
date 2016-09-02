f97e9472da8d4cb74e762e9b7499ad0c64182349
Leizxy <359844413@qq.com>
2016-09-02 17:41:11 +0800

    filter

--- DanmuView.lua
+++ DanmuView.lua
@@ -45,7 +45,9 @@ local FONT = {"Fonts\\ARHei.ttf",DANMU_FONT_SIZE,"OUTLINE"}
 local DURATION = 25  -- 弹幕在显示区域划过的时间。建议20s以上
 local COUNT_V = 10
 local SHOW_DANMU = true -- 是否打开弹幕(再加上频道过滤？)
-
+local MSG_FILTER = {
+"大脚任务提示："
+}
 
 local function AddAnimToDanmu(frame)
 	-- print("AddAnimToDanmu")
@@ -55,12 +57,15 @@ local function AddAnimToDanmu(frame)
 	translation:SetOffset(-UIWidth-frame:GetWidth(),0)
 	translation:SetTarget(frame.text)
 	-- translation:SetStartDelay(0.5)
-	translation:SetSmoothing("NONE")
+	translation:SetSmoothing("OUT")
 	frame.group:SetLooping("NONE")
 	frame.group:Play()
 	frame:SetScript("OnUpdate",function(self,t)
 		if translation:IsDone() then
-			-- table.remove(danmu,gsub(frame:GetName(),"danmu",""))
+			if frame ~= nil then
+				table.remove(danmu,gsub(frame:GetName(),"danmu",""))
+				table.remove(danmuText,gsub(frame:GetName(),"danmu",""))
+			end
 		end
 	end)
 end
@@ -82,40 +87,55 @@ local function CreateDanmu(i,...)
 	danmu[i].text:SetPoint("TOPLEFT",DanmuView,"TOPRIGHT",0, -((i%COUNT_V == 0 and COUNT_V or i%COUNT_V)*40 + 50)) --坐标
 	AddAnimToDanmu(danmu[i])
 end
--- /dump _G["danmuText"][1]
+-- /dump _G["danmu1"]
 -- /dump _G["GuildRosterContainerButton12String1"]:SetWidth(30)
 -- OrderHallCommandBar
+local function filter(text,tb)
+	local boolean = true
+	for i = 1, #tb do
+		boolean = boolean and not strfind(text,tb[1])
+	end
+	return boolean
+end
 
-local i = 1
 local ChatFrame1 = _G["ChatFrame1"]
 local add = ChatFrame1.AddMessage
 ChatFrame1.AddMessage = function(self,text,...)
+	-- shortChannel
+	if strfind(text, "大脚世界频道") then
+		text = gsub(text, "%[%d+%. .?.?.?.?.?.?(.-).?.?.?.?.?.?%]","[%1]") -- [世界]
+		-- text = gsub(text, "%[(.-)%. .?.?.?.?(.-).?.?.?.?%]","[%1.%2]") -- [1.世界]
+	else
+		text = gsub(text,"%d%. (.?.?.?).-%]","%1]")
+		-- text = gsub(text, "%[%d+%. (.?.?.?).+%]","[%1]") -- [综]
+		-- text = gsub(text, "%[%d+%. .+%]%[(.-)%]","[%1][%2]") -- [综]
+		-- text = gsub(text, "%[(.-)%. (.?.?).+%]","[%1.%2]")) -- [1. 综]
+	end
 	if (select(1,...)) then
 		if 
-		strfind(text,"%].-说：") or 
-		strfind(text,"发送.-%[.-") or 
-		strfind(text,"%]喊：") or 
-		strfind(text,"%[.-队.-%].-%[") or 
-		strfind(text,"%[公会%].-%[") or
-		strfind(text,"%[官员%].-%[") 
+		strfind(text,"%[.-%].-说：") or -- SAY,WHISPER(from)
+		strfind(text,"发送.-%[.-%]") or -- WHISPER(to)
+		strfind(text,"%[.-%]喊：") or -- YELL
+		strfind(text,"%[.-队.-%].-%[-.%]") or -- PARTY(LEADER),RAID(LEADER,WARNING)
+		strfind(text,"%[公会%].-%[-.%]") or -- GUILD
+		strfind(text,"%[官员%].-%[-.%]") -- OFFICER
 		then
-			if SHOW_DANMU then
+			if SHOW_DANMU 
+			-- and not (strfind(text,"过滤条件"))
+			-- and not (strfind(text,"大脚任务提示："))
+			and filter(text,MSG_FILTER)
+			then
+				local i = 1
+				-- i = i + 1
+				-- test
+				while danmu[i] == nil do
+					i = i + 1
+				end
 				danmuText[i] = text
 				CreateDanmu(i,...)
-				i = i + 1
 			end
 		end
 	end
-	-- shortChannel
-	if strfind(text, "大脚世界频道") then
-			text = gsub(text, "%[%d+%. .?.?.?.?.?.?(.-).?.?.?.?.?.?%]","[%1]") -- [世界]
-			-- text = gsub(text, "%[(.-)%. .?.?.?.?(.-).?.?.?.?%]","[%1.%2]") -- [1.世界]
-		else
-			text = gsub(text,"%d%. (.?.?.?).-%]","%1]")
-			-- text = gsub(text, "%[%d+%. (.?.?.?).+%]","[%1]") -- [综]
-			-- text = gsub(text, "%[%d+%. .+%]%[(.-)%]","[%1][%2]") -- [综]
-			-- text = gsub(text, "%[(.-)%. (.?.?).+%]","[%1.%2]")) -- [1. 综]
-		end
 	return add(self,text,...)
 end
 
